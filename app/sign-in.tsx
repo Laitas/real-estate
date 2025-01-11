e5e72877b367f986605107390fcc3af70e06a7df
Laitas <61273117+Laitas@users.noreply.github.com>
2025-01-11 16:09:03 +0200

    SignIn UI

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from "react-native";
+import images from "@/constants/images";
 import React from "react";
+import { Image, Pressable, ScrollView, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Text from "@/components/Text";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10 pb-6">
+          <Text className="text-base text-center uppercase text-black">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-secondary-300 text-center mt-2 capitalize">
+            Let's get you closer to {"\n"}
+            <Text className="text-primary-300 font-rubik-bold">
+              Your ideal home
+            </Text>
+          </Text>
+          <Text className="text-lg text-secondary-200 text-center mt-12">
+            Login to ReState with Google
+          </Text>
+          <Pressable
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 px-2 mt-5 flex flex-row items-center justify-center gap-2"
+          >
+            <Image
+              source={icons.google}
+              className="size-6"
+              resizeMode="contain"
+            />
+            <Text className="font-rubik-medium text-secondary-300 text-lg">
+              Continue with Google
+            </Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
